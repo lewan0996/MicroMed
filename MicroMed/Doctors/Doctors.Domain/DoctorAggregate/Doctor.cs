c757a82927d6627ced92dde504507d170ccb974b
Marcin Lewandowski <marcin.lewandowski@britishcouncil.org>
2023-12-27 21:27:13 +0100

    Doctor entity

---
+++ MicroMed/Doctors/Doctors.Domain/DoctorAggregate/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Domain;
+
+namespace Doctors.Domain.DoctorAggregate;
+
+public class Doctor : Entity<int>, IAggregateRoot
+{
+    public required Name Name { get; init; }
+    public required Specialty Specialty { get; init; }
+
+    public Doctor(Name name, Specialty specialty)
+    {
+        Name = name;
+        Specialty = specialty;
+    }
+
+    protected Doctor() { }
+}
+
+public enum Specialty
+{
+    Internist = 1,
+    Pediatrician = 2,
+    Dermatologist = 3,
+    Surgeon = 4,
+    Nurse = 5,
+    Neurologist = 6,
+    Cardiologist = 7,
+    Orthopaedist = 8
+}
